b10e3930bc3892311c84fa064b5193b492cb193f
Rodrigo <101999518+rdGxd@users.noreply.github.com>
2023-05-18 15:51:50 -0300

    Aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,27 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from "@fastify/jwt";
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
-})
+});
 
-app.register(memoriesRoutes)
+app.register(jwt, {
+  secret: "as1das1das0651da6s50d4056as4d065sa0a4s56das465asd456",
+});
+
+app.register(authRoutes);
+app.register(memoriesRoutes);
 
 app
   .listen({
     port: 3333,
+    host: "0.0.0.0"
   })
   .then(() => {
     console.log('🚀 HTTP server running on port http://localhost:3333')
